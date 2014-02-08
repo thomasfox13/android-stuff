0362b54106c67ea53a4212b8c14ef623d9b46240
thomasfox13 <thomas.fox@zoocasa.cam>
2014-02-08 03:51:00 -0500

    added About activity

--- Sudoku/src/com/example/sudoku/Sudoku.java
+++ Sudoku/src/com/example/sudoku/Sudoku.java
@@ -3,15 +3,36 @@ package com.example.sudoku;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.content.Intent;
 
-public class Sudoku extends Activity {
+public class Sudoku extends Activity implements OnClickListener {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sudoku);
+    
+        // Set up click listeners gor all the buttons
+        View continueButton = findViewById(R.id.continue_button);
+        continueButton.setOnClickListener(this);
+        View newButton = findViewById(R.id.new_button);
+        newButton.setOnClickListener(this);
+        View aboutButton = findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(this);
+        View exitButton = findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(this);
     }
-
+    
+    public void onClick(View v) {
+    	switch (v.getId()) {
+    	case R.id.about_button:
+    		Intent i = new Intent(this, About.class);
+    		startActivity(i);
+    		break;
+    	}
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
