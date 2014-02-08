527b56a0980ab7ee14dc538efcd33441a05610f5
thomasfox13 <thomas.fox@zoocasa.com>
2014-02-08 12:11:57 -0500

    added settings menu

---
+++ Sudoku/src/com/example/sudoku/NewGame.java
@@ -0,0 +1,12 @@
+package com.example.sudoku;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewGame extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.new_game);
+	}
+}
--- Sudoku/src/com/example/sudoku/Sudoku.java
+++ Sudoku/src/com/example/sudoku/Sudoku.java
@@ -3,6 +3,8 @@ package com.example.sudoku;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.content.Intent;
@@ -12,7 +14,7 @@ public class Sudoku extends Activity implements OnClickListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sudoku);
+        setContentView(R.layout.main);
     
         // Set up click listeners gor all the buttons
         View continueButton = findViewById(R.id.continue_button);
@@ -26,19 +28,41 @@ public class Sudoku extends Activity implements OnClickListener {
     }
     
     public void onClick(View v) {
+    	Class<?> which_class = null;
     	switch (v.getId()) {
-    	case R.id.about_button:
-    		Intent i = new Intent(this, About.class);
-    		startActivity(i);
+     	case R.id.continue_button:
     		break;
+    	case R.id.new_button:
+    		which_class = NewGame.class;
+    		break;
+    	case R.id.about_button:
+    		which_class = About.class;
+    		break;
+    	case R.id.exit_button:
+    		finish();
+    	}
+    	if (which_class != null) {
+        	Intent i = new Intent(this, which_class);
+        	startActivity(i);
     	}
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.sudoku, menu);
-        return true;
+    	super.onCreateOptionsMenu(menu);
+    	MenuInflater inflater = getMenuInflater();
+    	inflater.inflate(R.menu.menu, menu);
+    	return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+    	case R.id.settings:
+    		startActivity(new Intent(this, Prefs.class));
+    		return true;
+    	}
+    	return false;
     }
     
 }
